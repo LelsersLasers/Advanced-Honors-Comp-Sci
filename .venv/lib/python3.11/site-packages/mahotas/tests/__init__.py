e5beec0518f975ba13322ce48c31bd4e09910f1b
Lord-Lelsers <69917973+Lord-Lelsers@users.noreply.github.com>
2023-08-24 16:21:30 -0700

    Accidently pushed .venv

--- .venv/lib/python3.11/site-packages/mahotas/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-def run(verbose=False):
-    from os import path
-    import pytest
-    args = [path.dirname(__file__)]
-    if verbose:
-        args.append('--verbose')
-    return pytest.main(args)
--- .venv/lib/python3.11/site-packages/mahotas/tests/test_demos.py
+++
@@ -1,7 +0,0 @@
-import mahotas.demos
-from os import path
-
-def test_image_path():
-    assert path.exists(mahotas.demos.image_path('luispedro.jpg'))
-    assert not path.exists(mahotas.demos.image_path('something-that-does-not-exist'))
-
