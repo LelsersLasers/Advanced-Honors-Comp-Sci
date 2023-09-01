959153125b14ffb513c4e6e0e746f691943089e4
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-09-01 09:30:23 -0700

    Day 9: Through 11.4

---
+++ book/contours.py
@@ -0,0 +1,32 @@
+import numpy as np
+import argparse
+import imutils
+import mahotas # for Otsu and Riddler-Calvard
+import cv2
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to image")
+args = vars(ap.parse_args())
+
+
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+# blurred = cv2.bilateralFilter(gray, 7, 41, 41),
+
+cv2.imshow("Image", blurred)
+
+edged = cv2.Canny(blurred, 30, 150)
+cv2.imshow("Edges", edged)
+
+(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+print("# of countors", len(cnts))
+
+coins = image.copy()
+cv2.drawContours(coins, cnts, -1, (0, 255, 0), 2)
+cv2.imshow("Coins", coins)
+
+cv2.waitKey(10000)
+
