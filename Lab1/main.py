0627cd270905aac0d3495dc8b722ee7df53d2146
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2023-09-08 18:48:00 -0700

    Post Retreat: Blurring parameters changed; blur + oval works; formatting code

--- Lab1/main.py
+++ Lab1/main.py
@@ -4,7 +4,7 @@ import cv2
 import sys
 import time
 
-#------------------------------------------------------------------------------#
+# ---------------------------------------------------------------------------- #
 """
 - Image basics: saving/loading
 - Drawing: rectangle in debug mode
@@ -20,12 +20,14 @@ TODO:
     - Map each face to a number and the number to the center of the face
     - Organize faces that minimizes the total distance between the new centers of the face and the old centers of the face
 - Cut faces out as ovals
+    - +Play nice with blurring
 - Smoothing and blurring: WIP
     - Create blurred version of output frame
     - Create mask for the outline of the pasted face
     - Used blurred image on masked areas, and original on non-masked areas
     - Make argparse option
-        - `-b X` -> blur with X px radius
+        - `-r` or `--radius` for blur radius in blur function
+        - `-b` or `--blur` for blur size in blur edges
     - Make it blur in "layers"
         - Blur the edges with a very strong blur, then blur the surrounding area with a weaker blur
 - Forground extraction: Worth it? (Very slow, and iffy)
@@ -36,10 +38,11 @@ TODO:
 
 """
 
-#------------------------------------------------------------------------------#
+# ---------------------------------------------------------------------------- #
 modelFile = "models/res10_300x300_ssd_iter_140000_fp16.caffemodel"
 configFile = "models/deploy.prototxt"
 net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+
 
 class Face:
     def __init__(self, face):
@@ -55,18 +58,22 @@ class Face:
         return self.w * self.h
 
     def __str__(self):
-        return f'Face at {self.center}'
-    
-    def draw(self, image, color = (255, 0, 0)):
+        return f"Face at {self.center}"
+
+    def draw(self, image, color=(255, 0, 0)):
         # rect outline
-        cv2.rectangle(image, (self.x, self.y), (self.x+self.w, self.y+self.h), color, 2)
-        
+        cv2.rectangle(
+            image, (self.x, self.y), (self.x + self.w, self.y + self.h), color, 2
+        )
+
+
 def detect_faces(image, conf):
     faces = detect_faces_dnn(image, conf)
     faces.sort(key=Face.calc_area, reverse=True)
-    
+
     return faces
-    
+
+
 def detect_faces_dnn(image, conf):
     blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), [104, 117, 123], False, False)
 
@@ -96,10 +103,11 @@ def detect_faces_dnn(image, conf):
 
             # detected_faces.append(Face((x, y, expanded_w, expanded_h)))
             detected_faces.append(Face((x1, y1, w, h)))
-    
+
     return detected_faces
 
-#------------------------------------------------------------------------------#
+
+# ---------------------------------------------------------------------------- #
 def combine_with_mask(image1, image2, mask):
     # When mask = true => image2, else image1
     inverse_mask = cv2.bitwise_not(mask)
@@ -109,31 +117,48 @@ def combine_with_mask(image1, image2, mask):
 
     return final_result
 
+
 def swap_faces(original_image, output_image, faces, oval):
     for i, face in enumerate(faces):
-        try:    
-            face_crop = original_image[face.y:face.y+face.h, face.x:face.x+face.w]
-            next_face = faces[(i+1) % len(faces)]
+        try:
+            face_crop = original_image[
+                face.y : face.y + face.h, face.x : face.x + face.w
+            ]
+            next_face = faces[(i + 1) % len(faces)]
             face_resized = cv2.resize(face_crop, (next_face.w, next_face.h))
 
             if oval:
-                # oval_mask = np.zeros(output_image.shape[:2], np.uint8)
-                # cv2.ellipse(oval_mask, (int(next_face.center_x), int(next_face.center_y)), (int(next_face.w / 2), int(next_face.h / 2)), 0, 0, 360, (255, 255, 255), -1)
-                # # TODO:
-
-                # cv2.imshow("oval_mask", oval_mask)
-
-                # oval_face = np.zeros(output_image.shape[:2], np.uint8)
-
                 oval_mask = np.zeros(face_resized.shape[:2], np.uint8)
-                cv2.ellipse(oval_mask, (int(next_face.w / 2), int(next_face.h / 2)), (int(next_face.w / 2), int(next_face.h / 2)), 0, 0, 360, (255, 255, 255), -1)
-                face_resized = cv2.bitwise_and(face_resized, face_resized, mask=oval_mask)
+                cv2.ellipse(
+                    oval_mask,
+                    (int(next_face.w / 2), int(next_face.h / 2)),
+                    (int(next_face.w / 2), int(next_face.h / 2)),
+                    0,
+                    0,
+                    360,
+                    (255, 255, 255),
+                    -1,
+                )
+                face_resized = cv2.bitwise_and(
+                    face_resized, face_resized, mask=oval_mask
+                )
 
-                output_image[next_face.y:next_face.y+next_face.h, next_face.x:next_face.x+next_face.w] = combine_with_mask(output_image[next_face.y:next_face.y+next_face.h, next_face.x:next_face.x+next_face.w], face_resized, oval_mask)
+                output_image[
+                    next_face.y : next_face.y + next_face.h,
+                    next_face.x : next_face.x + next_face.w,
+                ] = combine_with_mask(
+                    output_image[
+                        next_face.y : next_face.y + next_face.h,
+                        next_face.x : next_face.x + next_face.w,
+                    ],
+                    face_resized,
+                    oval_mask,
+                )
             else:
-                output_image[next_face.y:next_face.y+next_face.h, next_face.x:next_face.x+next_face.w] = face_resized
-
-
+                output_image[
+                    next_face.y : next_face.y + next_face.h,
+                    next_face.x : next_face.x + next_face.w,
+                ] = face_resized
 
             # Grapcut 2
             # mask = np.zeros(face_resized.shape[:2], np.uint8)
@@ -148,18 +173,40 @@ def swap_faces(original_image, output_image, faces, oval):
 
             # face_grapcut = face_resized * mask2[:, :, np.newaxis]
             # cv2.imshow("face_grapcut", face_grapcut)
-            
+
         except:
             pass
 
-def blur_edges(output_image, faces, blur_radius):
+
+def blur_edges(output_image, faces, blur_thickness, blur_radius, oval):
     # blurred_image = cv2.blur(output_image, (51, 51))
+
+    if blur_radius % 2 == 0:
+        blur_radius += 1
     blurred_image = cv2.medianBlur(output_image, blur_radius)
 
     mask = np.zeros(output_image.shape[:2], np.uint8)
 
     for face in faces:
-        cv2.rectangle(mask, (face.x, face.y), (face.x+face.w, face.y+face.h), (255, 255, 255), blur_radius * 2)
+        if oval:
+            cv2.ellipse(
+                mask,
+                (int(face.center_x), int(face.center_y)),
+                (int(face.w / 2), int(face.h / 2)),
+                0,
+                0,
+                360,
+                (255, 255, 255),
+                blur_thickness,
+            )
+        else:
+            cv2.rectangle(
+                mask,
+                (face.x, face.y),
+                (face.x + face.w, face.y + face.h),
+                (255, 255, 255),
+                blur_thickness,
+            )
 
     # inverse_mask = cv2.bitwise_not(mask)
     # result = cv2.bitwise_and(output_image, output_image, mask=inverse_mask)
@@ -170,27 +217,37 @@ def blur_edges(output_image, faces, blur_radius):
 
     return final_result
 
-def double_blur_edges(output_image, faces, blur):
-    blur_first = blur if blur % 2 == 1 else blur + 1
-    blur_second = blur * 2 if (blur * 2) % 2 == 1 else blur * 2 + 1
 
-    output_image = blur_edges(output_image, faces, blur_first)
-    output_image = blur_edges(output_image, faces, blur_second)
+# def double_blur_edges(output_image, faces, blur):
+#     blur_first = blur if blur % 2 == 1 else blur + 1
+#     blur_second = blur * 2 if (blur * 2) % 2 == 1 else blur * 2 + 1
 
-    return output_image
+#     output_image = blur_edges(output_image, faces, blur_first)
+#     output_image = blur_edges(output_image, faces, blur_second)
 
-def video_detection(orginal_video, save, conf, debug, blur, oval):
+#     return output_image
+
+
+def video_detection(
+    orginal_video, save, conf, debug, blur, blur_thickness, blur_radius, oval
+):
     if not orginal_video.isOpened():
         raise Exception("Could not open video")
-    
+
     if save is not None:
         fps = orginal_video.get(cv2.CAP_PROP_FPS)
         width = int(orginal_video.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(orginal_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        fourcc = int(orginal_video.get(cv2.CAP_PROP_FOURCC)).to_bytes(4, byteorder=sys.byteorder).decode()
+        fourcc = (
+            int(orginal_video.get(cv2.CAP_PROP_FOURCC))
+            .to_bytes(4, byteorder=sys.byteorder)
+            .decode()
+        )
 
-        output_video = cv2.VideoWriter(save, cv2.VideoWriter_fourcc(*fourcc), fps, (width, height))
+        output_video = cv2.VideoWriter(
+            save, cv2.VideoWriter_fourcc(*fourcc), fps, (width, height)
+        )
 
     while orginal_video.isOpened():
         ret, frame = orginal_video.read()
@@ -201,9 +258,10 @@ def video_detection(orginal_video, save, conf, debug, blur, oval):
         output_frame = frame.copy()
         faces = detect_faces(frame, conf)
         swap_faces(frame, output_frame, faces, oval)
-        if blur is not None:
-            output_frame = double_blur_edges(output_frame, faces, blur)
-
+        if blur:
+            output_frame = blur_edges(
+                output_frame, faces, blur_thickness, blur_radius, oval
+            )
 
         if debug:
             for face in faces:
@@ -214,10 +272,9 @@ def video_detection(orginal_video, save, conf, debug, blur, oval):
 
         cv2.imshow("Output", output_frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
-    
     orginal_video.release()
 
     if save is not None:
@@ -225,16 +282,31 @@ def video_detection(orginal_video, save, conf, debug, blur, oval):
 
     cv2.destroyAllWindows()
 
-def image_detection(original_image, output_image, save, conf, debug, blur, oval):
+
+def image_detection(
+    original_image,
+    output_image,
+    save,
+    conf,
+    debug,
+    blur,
+    blur_thickness,
+    blur_radius,
+    oval,
+):
     faces = detect_faces(original_image, conf)
-    
+
     if len(faces) < 2:
-        raise Exception(f'Not enough faces detected. {len(faces)} detected, at least 2 required.')
-    
+        raise Exception(
+            f"Not enough faces detected. {len(faces)} detected, at least 2 required."
+        )
+
     swap_faces(original_image, output_image, faces, oval)
 
-    if blur is not None:
-        output_image = double_blur_edges(output_image, faces, blur)
+    if blur:
+        output_image = blur_edges(
+            output_image, faces, blur_thickness, blur_radius, oval
+        )
 
     if debug:
         for face in faces:
@@ -243,26 +315,63 @@ def image_detection(original_image, output_image, save, conf, debug, blur, oval)
     if save is not None:
         cv2.imwrite(save, output_image)
 
-
     cv2.imshow("Output", output_image)
 
     cv2.waitKey(10_000)
-#------------------------------------------------------------------------------#
 
 
-#------------------------------------------------------------------------------#
+# ---------------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------------- #
 ap = argparse.ArgumentParser()
 
 
-ap.add_argument("-i", "--input", required = True, help = "input type", choices=["image", "video", "camera"])
+# TODO: use type=
+ap.add_argument(
+    "-i",
+    "--input",
+    required=True,
+    help="input type",
+    choices=["image", "video", "camera"],
+)
+ap.add_argument(
+    "-p",
+    "--path",
+    required=False,
+    help="path to image or video (not required for camera)",
+    default=None,
+)
+ap.add_argument(
+    "-c",
+    "--confidence",
+    required=False,
+    help="confidence threshold for face detection",
+    default=0.8,
+    type=float,
+)
+ap.add_argument(
+    "-s", "--save", required=False, help="save output to path", default=None
+)
+ap.add_argument(
+    "-d", "--debug", required=False, help="draw debug outlines", action="store_true"
+)
+# TODO: clean wording for blur
+ap.add_argument(
+    "-b", "--blur", required=False, help="enable blurring", action="store_true"
+)
+ap.add_argument(
+    "-t", "--blur-thickness", required=False, help="blur thickness", default=40
+)
+ap.add_argument("-r", "--blur-radius", required=False, help="blur strength", default=21)
 
-ap.add_argument("-p", "--path", required = False, help = "path to image or video (not required for camera)", default=None)
-
-ap.add_argument("-c", "--confidence", required = False, help = "confidence threshold for face detection", default=0.8, type=float)
-ap.add_argument("-s", "--save", required = False, help = "save output to path", default=None)
-ap.add_argument("-d", "--debug", required = False, help = "draw debug outlines", action="store_true")
-ap.add_argument("-b", "--blur", required = False, help = "blur edges radius", default = None)
-ap.add_argument("-o", "--oval", required = False, help = "cut out faces as ovals instead of rectangles", action="store_true")
+ap.add_argument(
+    "-o",
+    "--oval",
+    required=False,
+    help="cut out faces as ovals instead of rectangles",
+    action="store_true",
+)
 # TODO: command for face mapping order
 args = vars(ap.parse_args())
 
@@ -279,22 +388,35 @@ debug = args["debug"]
 oval = args["oval"]
 
 blur = args["blur"]
-if blur is not None:
-    blur = int(blur)
+blur_thickness = int(args["blur_thickness"])
+blur_radius = int(args["blur_radius"])
 
 
 print("Press q to quit")
 
+# TODO: clean stuff argparse results into a struct or something
 if input_type == "image":
     original_image = cv2.imread(path)
     output_image = original_image.copy()
-    image_detection(original_image, output_image, save, conf, debug, blur, oval)
+    image_detection(
+        original_image,
+        output_image,
+        save,
+        conf,
+        debug,
+        blur,
+        blur_thickness,
+        blur_radius,
+        oval,
+    )
 elif input_type == "video":
     original_video = cv2.VideoCapture(path)
-    video_detection(original_video, save, conf, debug, blur, oval)
+    video_detection(
+        original_video, save, conf, debug, blur, blur_thickness, blur_radius, oval
+    )
 elif input_type == "camera":
     original_video = cv2.VideoCapture(0)
     time.sleep(0.1)
-    video_detection(original_video, save, conf, debug, blur, oval)
-    
-    
+    video_detection(
+        original_video, save, conf, debug, blur, blur_thickness, blur_radius, oval
+    )
