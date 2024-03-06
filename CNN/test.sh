7e12593fe7d2168895db7ad336a391820a8b999c
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-03-06 09:22:05 -0800

    Lowering regularization

--- CNN/test.sh
+++ CNN/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python learn.py -e 20 -c output4/checkpoint -s output4/fullsave -p output4/graph.json --l1 0.05 --l2 0.05
+python learn.py -e 20 -c output7/checkpoint -s output7/fullsave -p output7/graph.json --l1 0.01 --l2 0.01
