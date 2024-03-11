ad289f3b5f42193addbe4e6c3b091c26c3443f17
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-03-11 08:32:08 -0700

    Testing augmentation changes

--- CNN/test.sh
+++ CNN/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python learn.py -e 20 -c output7/checkpoint -s output7/fullsave -p output7/graph.csv -i 0
+python learn.py -e 100 -c output7/checkpoint -s output7/fullsave -p output7/graph.csv
