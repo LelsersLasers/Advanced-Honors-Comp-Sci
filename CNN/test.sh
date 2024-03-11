bdb2ef83634af3a0e597fadebae98d711e3544a0
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-03-11 11:02:25 -0700

    Moving output files into more nested folder

--- CNN/test.sh
+++ CNN/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python learn.py -e 100 -c output7/checkpoint -s output7/fullsave -p output7/graph.csv
+python learn.py -e 100 -c output/output8/checkpoint -s output/output8/fullsave -p output/output8/graph.csv
