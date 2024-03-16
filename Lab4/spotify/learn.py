d60d3b7ac25477aa1aebdd089510fa72052da3b8
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-03-15 18:16:34 -0700

    Trying to use TensorFlow Similarity

--- Lab4/spotify/learn.py
+++ Lab4/spotify/learn.py
@@ -1,29 +1,39 @@
+
+
+# ---------------------------------------------------------------------------- #
 import pandas as pd
 import numpy as np
 
 np.set_printoptions(precision=3, suppress=True)
+# ---------------------------------------------------------------------------- #
 
-# import tensorflow as tfs
+
+# ---------------------------------------------------------------------------- #
+import tensorflow as tf
 # import tensorflow.data as data
-import tensorflow.keras as keras
+# import tensorflow.keras as keras
 # import tensorflow.keras.utils as utils
 import tensorflow.keras.layers as layers
-import tensorflow.keras.losses as losses
+# import tensorflow.keras.losses as losses
 import tensorflow.keras.optimizers as optimizers
 import tensorflow.keras.activations as activations
 
+import tensorflow_similarity as tfsim
+import tensorflow_similarity.losses as tfsim_losses
 
+print(f"\n\nTensorflow version: {tf.__version__}")
+print(f"TensorFlow Similarity version {tfsim.__version__}\n\n")
+# ---------------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------------- #
 DATA_PATH = 'data/data/data.csv'
-
 
 data = pd.read_csv(DATA_PATH)
 print(data.head())
 print(data.info())
 
 data_features = data.copy()
-
-# - Don't need: artists, explicit, id, mode, name
-# - Build similarity based on popularity
 
 data_labels = data_features.pop('popularity') # target
 
@@ -40,31 +50,42 @@ print(data_features.info())
 
 data_features = np.asarray(data_features).astype(np.float32)
 print(data_features)
+# ---------------------------------------------------------------------------- #
 
 
+# ---------------------------------------------------------------------------- #
+def make_model():
+	normalize = layers.Normalization()
+	normalize.adapt(data_features)
 
-normalize = layers.Normalization()
-normalize.adapt(data_features)
+	inputs = layers.Input(shape=(data_features.shape[1],))
+	x = normalize(inputs)
+	x = layers.Dense(256, activation=activations.relu)(x)
+	x = layers.Dense(256, activation=activations.relu)(x)
+	x = layers.Dense(256, activation=activations.relu)(x)
+	outputs = tfsim.layers.MetricEmbedding(100)(x)
 
-model = keras.Sequential([
-	normalize,
-	layers.Dense(64, activation=activations.relu),
-	layers.Dense(64, activation=activations.relu),
-	layers.Dense(1)
-])
+	model = tfsim.models.SimilarityModel(inputs, outputs)
+	return model
 
-print(model.summary())
+model = make_model()
 
 optimizer = optimizers.Adam(learning_rate = 0.001)
-loss = losses.MeanSquaredError()
+loss = tfsim_losses.MultiSimilarityLoss(distance='cosine')
 
 model.compile(
 	optimizer=optimizer,
 	loss=loss,
 )
 
+print(model.summary())
+# ---------------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------------- #
 model.fit(
 	data_features,
 	data_labels,
 	epochs=3
 )
+# ---------------------------------------------------------------------------- #
