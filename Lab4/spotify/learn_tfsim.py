1e051c10e9dead9e19c0f59ce6c04fd0edf30a34
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2024-03-17 15:11:21 -0700

    Moving data prep into learn file + naming changes

--- Lab4/spotify/learn_tfsim.py
+++ Lab4/spotify/learn_tfsim.py
@@ -1,7 +1,3 @@
-
-import data
-
-
 # ---------------------------------------------------------------------------- #
 import tensorflow as tf
 # import tensorflow.data as data
@@ -22,7 +18,44 @@ print(f"TensorFlow Similarity version {tfsim.__version__}\n\n")
 
 
 # ---------------------------------------------------------------------------- #
-data_features, data_labels = data.load_data()
+import pandas as pd
+import numpy as np
+
+np.set_printoptions(precision=3, suppress=True)
+
+DATA_PATH = 'data/data.csv'
+POPULARITY_COLUMN_COUNT = 10
+
+
+def load_data():
+	data = pd.read_csv(DATA_PATH)
+	print(data.head())
+	print(data.info())
+
+	data_features = data.copy()
+
+	pop_to_column = lambda x: int(x / POPULARITY_COLUMN_COUNT) + 1
+	data_features['popularity'] = data_features['popularity'].apply(pop_to_column)
+
+	data_labels = data_features.pop('popularity') # target
+
+	data_features.pop('artists')
+	data_features.pop('explicit')
+	data_features.pop('id')
+	data_features.pop('mode')
+	data_features.pop('name')
+	data_features.pop('release_date')
+
+
+	print(data_features.head())
+	print(data_features.info())
+
+	data_features = np.asarray(data_features).astype(np.float32)
+	print(data_features)
+
+	return data_features, data_labels
+
+data_features, data_labels = load_data()
 # ---------------------------------------------------------------------------- #
 
 
