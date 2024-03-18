c3dda2de497b8745391b8a69cb55f43233b1dc5b
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-03-18 08:02:50 -0700

    Testing current setup

--- Lab4/spotify/learn_predict_encoder.py
+++ Lab4/spotify/learn_predict_encoder.py
@@ -34,9 +34,9 @@ def load_data():
     data_features = np.asarray(data_features).astype(np.float32)
     print(data_features)
 
-    return data_features, data_labels
+    return data, data_features, data_labels
 
-data_features, data_labels = load_data()
+all_data, data_features, data_labels = load_data()
 # ---------------------------------------------------------------------------- #
 
 
@@ -59,9 +59,11 @@ def make_model():
     
     model = keras.Sequential([
         normalize,
-        layers.Dense(64, activation='relu'),
-        layers.Dense(64, activation='relu'),
-        layers.Dense(1), # linear regression so no activation
+        layers.Dense(8),
+        # layers.Dense(200),
+        # layers.Dense(100),
+        layers.Dense(4),
+        layers.Dense(1),
     ])
 
     loss = losses.MeanSquaredError()
@@ -73,7 +75,7 @@ def make_model():
 
 model = make_model()
 
-model.fit(data_features, data_labels, epochs=10)
+model.fit(data_features, data_labels, epochs=50)
 
 model.save('output/try2-fullsave')
 # ---------------------------------------------------------------------------- #
@@ -84,8 +86,11 @@ def extract_embedding(model, x):
     intermediate_model = keras.Model(inputs=model.inputs, outputs=model.layers[-2].output)
     return intermediate_model(x)
 
-test = '0.899,1926,0.995,["Louis Armstrong & His Hot Five"],0.614,189333,0.196,0,0jiH6Bf3OOm36ubMWZ0Sr5,0.892,4,0.0526,-14.019,1,Jazz Lips,9,1926,0.4270000000000001,201.11900000000003'
+def cosine_distance(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
+# test = '0.899,1926,0.995,["Louis Armstrong & His Hot Five"],0.614,189333,0.196,0,0jiH6Bf3OOm36ubMWZ0Sr5,0.892,4,0.0526,-14.019,1,Jazz Lips,9,1926,0.4270000000000001,201.11900000000003'
+test = '0.285,2000,0.00239,["Coldplay"],0.429,266773,0.6609999999999999,0,3AJwUDP919kvQ9QcozQPxg,0.000121,11,0.234,-7.227,1,Yellow,84,2000-07-10,0.0281,173.372'
 test = test.split(',')
 
 valence = float(test[0])
@@ -105,8 +110,33 @@ x = np.array([valence, year, acousticness, danceability, duration_ms, energy, in
 x = np.expand_dims(x, axis=0)
 print(x)
 
-embedding = extract_embedding(model, x)
-print(embedding)
+base_embedding = extract_embedding(model, x)
+print(base_embedding)
 
+print("Extracting all embeddings...")
+all_embeddings = []
+for i in range(len(data_features)):
+    all_embeddings.append(extract_embedding(model, np.expand_dims(data_features[i], axis=0)))
+print("Extracted all embeddings")
+
+print("Sorting by cosine distance...")
+angles = []
+for i in range(len(all_embeddings)):
+    b = np.squeeze(base_embedding.numpy(), axis=0)
+    a = np.squeeze(all_embeddings[i].numpy(), axis=0)
+    cos = cosine_distance(a, b)
+    angle = np.arccos(cos) * 180 / np.pi
+    angles.append(angle)
+sorted_indices = np.argsort(angles)
+print("Sorted by cosine distance")
+
+print("Top 10 most similar songs:")
+for i in range(20):
+    ind = sorted_indices[i]
+    angle = angles[ind]
+    title = all_data.iloc[ind]['name']
+    artist = all_data.iloc[ind]['artists']
+
+    print(f"{i+1}. {title} by {artist} ({angle:.2f}°)")
 
 
