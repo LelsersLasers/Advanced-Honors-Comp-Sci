63cc7b3ba311cd2435a06ad935577d453698e767
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-04-26 09:32:38 -0700

    Just making a giant page instead of stacking layouts

--- Lab4/frontend/src/routes/recommendations/+page.svelte
+++ Lab4/frontend/src/routes/recommendations/+page.svelte
@@ -1,3 +1,42 @@
+<script>
+	let method = "cnn";
+	let dist = "cos";
+
+
+</script>
+
+
+
+
+
 <svelte:head>
 	<title>Recommendations Home</title>
 </svelte:head>
+
+
+<a href="/">
+	<button>Back to Home</button>
+</a>
+
+<br />
+
+<h2>Method</h2>
+
+<label for="method">Method:</label>
+<select id="method" name="method" bind:value={method}>
+	<option value="cnn" selected="selected">CNN</option>
+	<option value="autoencoder">Autoencoder</option>
+	<option value="predictor">Predictor</option>
+	<option value="simple">Simple</option>
+</select>
+
+<br />
+
+<label for="dist">Distance Function:</label>
+<select id="dist" name="dist" bind:value={dist}>
+	<option value="cos" selected="selected">Cosine Similarity</option>
+	<option value="mae">Mean Absolute Difference</option>
+	<option value="mse">Mean Squared Difference</option>
+	<option value="euclidean">Euclidean Distance</option>
+	<option value="dot">Dot Product</option>
+</select>
