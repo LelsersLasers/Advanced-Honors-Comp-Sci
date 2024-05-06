56f74f98b0189ba8b2ba734aaeedf4a5cfb6187a
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-05-06 12:04:05 -0700

    Preparing to setup new autoencoder + embeddings

--- Lab4/main_build.py
+++ Lab4/main_build.py
@@ -3,12 +3,12 @@
 # data.cnn_data(False)
 
 
-import styles.simple
-styles.simple.predict()
+# import styles.simple
+# styles.simple.predict()
 
 
 import styles.predictor
-styles.predictor.train()
+# styles.predictor.train()
 styles.predictor.embeddings()
 styles.predictor.predict()
 
